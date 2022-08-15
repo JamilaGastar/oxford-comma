6a38ec2c68a17bb15b93a68ccf5dedc009de054f
jamilagastar <jamilagastar@gmail.com>
2022-08-15 22:05:37 +1000

    partially completed lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length() == 2
+        array.join(" and ")
+    elsif array.length() == 3
+        puts array.insert(2, "and")
+        .join(", ")
+    else
+        array.join
+    end
 end
+
+oxford_comma(["Apple", "Banana", "Pineapple"])
